a6b577f5c3b446d8ce03266bd4958154bd67a35b
bananakitssu <bananacool467+2@gmail.com>
2026-05-22 20:18:23 -0400

    Update

---
+++ libraries/Python/Air.py
@@ -0,0 +1,10 @@
+from AirData import AirData
+from Vector3 import Vector3
+
+class Air:
+    def __init__(self, data: AirData):
+        self._density = data.get("density") or 1.225
+        self._temperature = data.get("temperature") or 30
+        self._airArea = data.get("airArea") or Vector3(3000, 3000, 3000)
+        self._airWindForce = data.get("airWindForce") or 5
+        self._airWindDirection = data.get("airWindDirection") or Vector3(0, 0, 0)
---
+++ libraries/Python/AirData.py
@@ -0,0 +1,8 @@
+from typing import TypedDict, Optional
+from Vector3 import Vector3
+
+class AirData(TypedDict, total=False):
+    airArea: Vector3
+    airWindDirection: Vector3
+    airForce: float
+    density: float
