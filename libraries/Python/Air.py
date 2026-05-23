593b47a2649987a6c7db15485676a3632a48ce77
bananakitssu <bananacool467+2@gmail.com>
2026-05-22 20:28:43 -0400

    Update

--- libraries/Python/Air.py
+++ libraries/Python/Air.py
@@ -2,9 +2,49 @@ from AirData import AirData
 from Vector3 import Vector3
 
 class Air:
-    def __init__(self, data: AirData):
+    def __init__(self, data: AirData = {}):
         self._density = data.get("density") or 1.225
         self._temperature = data.get("temperature") or 30.0
         self._airArea = data.get("airArea") or Vector3(3000, 3000, 3000)
         self._airWindForce = data.get("airWindForce") or 5.0
         self._airWindDirection = data.get("airWindDirection") or Vector3(0, 0, 0)
+        
+    @property
+    def Density(self):
+        return self._density
+
+    @Density.setter
+    def Density(self, value: float):
+        self._density = value
+
+    @property
+    def Temperature(self):
+        return self._temperature
+
+    @Temperature.setter
+    def Temperature(self, value: float):
+        self._temperature = value
+
+    @property
+    def AirArea(self):
+        return self._airArea
+
+    @AirArea.setter
+    def AirArea(self, value: Vector3):
+        self._airArea = value
+
+    @property
+    def AirWindForce(self):
+        return self._airWindForce
+
+    @AirWindForce.setter
+    def AirWindForce(self, value: float):
+        self._airWindForce = value
+
+    @property
+    def AirWindDirection(self):
+        return self._airWindDirection
+
+    @AirWindDirection.setter
+    def AirWindDirection(self, value: Vector3):
+        self._airWindDirection = value
